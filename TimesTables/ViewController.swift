ebe5e357fbd5c1c2efa4af676a39a97b23a3e0fa
selveus1 <fs031090@gmail.com>
2018-02-05 17:00:42 -0500

    finished general product

--- TimesTables/ViewController.swift
+++ TimesTables/ViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let numsRows = 101
+    var num = 1
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var currentValue: UILabel!
+    @IBOutlet var table: UITableView!
+    
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        var intValue = Int(slider.value)
+        num = intValue
+        currentValue.text = String("Current Times Table : \(intValue)")
+        table.reloadData()
+    }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return numsRows
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String( "\(num) x \(indexPath.row) = \(num*(indexPath.row))" )
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
